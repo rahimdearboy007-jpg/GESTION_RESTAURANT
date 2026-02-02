6304f09204387bf1ab2eebc0c686eeef9c93ec6f
RVH1M <rahimdearboy007@gmail.com>
2026-02-02 01:04:43 +0000

    Initial commit

---
+++ src/gest_restaurant/GEST_RESTAURANT.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gest_restaurant;
+
+/**
+ *
+ * @author rahim
+ */
+public class GEST_RESTAURANT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
